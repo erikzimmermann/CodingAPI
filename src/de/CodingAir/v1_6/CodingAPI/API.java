22675b3162aa5f088dec90750c1670b84fde8360
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2017-08-12 16:11:37 +0200

    Revert "Test"
    This reverts commit d9fe4881af8161be0a3fae69da3154463bc2c8a9.

--- src/de/CodingAir/v1_6/CodingAPI/API.java
+++ src/de/CodingAir/v1_6/CodingAPI/API.java
@@ -30,6 +30,7 @@ import java.util.List;
 
 public class API {
 	private static final List<Removable> REMOVABLES = new ArrayList<>();
+	private static boolean editing = false;
 	
 	private static API instance;
 	public static final String VERSION = "v1.6";
