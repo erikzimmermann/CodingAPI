49f307298f3677ac1a307565370a5ef26e4fa68f
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2019-09-18 13:25:58 +0200

    Improves json methods

--- src/de/codingair/codingapi/tools/JSON/JSONObject.java
+++ src/de/codingair/codingapi/tools/JSON/JSONObject.java
@@ -13,11 +13,14 @@ public class JSONObject extends org.json.simple.JSONObject {
     @Override
     public Object put(Object key, Object value) {
         if(value == null) return remove(key);
-        else return super.put(key, value);
+        else {
+            if(value.getClass().isEnum()) value = value.toString();
+            return super.put(key, value);
+        }
     }
 
     public <T> T get(String key) {
-        return get(key, null);
+        return get(key, (T) null);
     }
 
     public <T> T get(String key, T def) {
@@ -29,5 +32,19 @@ public class JSONObject extends org.json.simple.JSONObject {
         }
 
         return o == null ? def : (T) o;
+    }
+
+    public <T extends Enum<T>> T get(String key, Class<T> def) {
+        Object o = super.get(key);
+
+        if(o == null) return null;
+        if(!(o instanceof String)) throw new IllegalArgumentException("Value isn't a String. Can't search for a enum!");
+        String name = (String) o;
+
+        for(T e : def.getEnumConstants()) {
+            if(e.name().equals(name)) return e;
+        }
+
+        return null;
     }
 }
