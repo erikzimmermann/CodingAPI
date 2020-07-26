8ee830a2a47927328fcf36f38be867bac882fc25
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2020-07-26 13:10:27 +0200

    Removes Countdown class

--- src/de/codingair/codingapi/time/CountdownListener.java
+++
@@ -1,8 +0,0 @@
-package de.codingair.codingapi.time;
-
-public abstract class CountdownListener {
-	protected abstract void CountdownStartEvent();
-	protected abstract void CountdownEndEvent();
-	protected abstract void CountdownCancelEvent();
-	protected abstract void onTick(int timeLeft);
-}
--- src/de/codingair/codingapi/time/Timer.java
+++ src/de/codingair/codingapi/time/Timer.java
@@ -1,27 +1,19 @@
 package de.codingair.codingapi.time;
 
 
-
 public class Timer {
-	private double lastStoppedTime = 0;
-	private long currentTime = 0;
-	
-	public void start(){
-		this.currentTime = System.currentTimeMillis();
-	}
-	
-	public void stop(){
-		this.lastStoppedTime = (double) (System.currentTimeMillis() - this.currentTime) / 1000;
-		int temp = (int) (this.lastStoppedTime * 1000);
-		this.lastStoppedTime = temp;
-		this.lastStoppedTime /= 1000;
-	}
+    private double lastStoppedTime = 0;
+    private long start = 0;
 
-	/**
-	 * Time is in MiliSeconds
-	 * @return
-	 */
-	public double getLastStoppedTime() {
-		return lastStoppedTime;
-	}
+    public void start() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.lastStoppedTime = ((int) (System.currentTimeMillis() - this.start)) / 1000;
+    }
+
+    public double getLastStoppedTime() {
+        return lastStoppedTime;
+    }
 }
