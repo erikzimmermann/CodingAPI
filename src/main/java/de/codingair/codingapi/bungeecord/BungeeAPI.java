72844bd442b98dce2fdf0e06292819bf570642a3
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2020-07-28 13:59:54 +0200

    Init maven

---
+++ src/main/java/de/codingair/codingapi/bungeecord/BungeeAPI.java
@@ -0,0 +1,33 @@
+package de.codingair.codingapi.bungeecord;
+
+import de.codingair.codingapi.bungeecord.listeners.ChatButtonListener;
+import de.codingair.codingapi.server.reflections.IReflection;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeAPI {
+    private static BungeeAPI instance;
+    private Plugin plugin;
+
+    public void onEnable(Plugin plugin) {
+        if(this.plugin != null) return;
+        this.plugin = plugin;
+
+        plugin.getProxy().getPluginManager().registerListener(plugin, (Listener) IReflection.getConstructor(ChatButtonListener.class).newInstance());
+    }
+
+    public void onDisable(Plugin plugin) {
+        plugin.getProxy().getPluginManager().unregisterListeners(plugin);
+
+        if(this.plugin.equals(plugin)) this.plugin = null;
+    }
+
+    public static BungeeAPI getInstance() {
+        if(instance == null) instance = new BungeeAPI();
+        return instance;
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+}
