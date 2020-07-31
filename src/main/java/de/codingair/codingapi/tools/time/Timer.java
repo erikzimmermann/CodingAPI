52009d7fc38e202c1f7f6cf5056b46fa8fbdbb9e
Erik Zimmermann <erik.zimmermann2000@yahoo.de>
2020-07-31 13:28:30 +0200

    Fixes timer

--- src/main/java/de/codingair/codingapi/tools/time/Timer.java
+++ src/main/java/de/codingair/codingapi/tools/time/Timer.java
@@ -2,18 +2,17 @@ package de.codingair.codingapi.tools.time;
 
 
 public class Timer {
-    private double lastStoppedTime = 0;
     private long start = 0;
 
     public void start() {
         this.start = System.currentTimeMillis();
     }
 
-    public void stop() {
-        this.lastStoppedTime = ((int) (System.currentTimeMillis() - this.start)) / 1000;
+    public long stop() {
+        return System.currentTimeMillis() - this.start;
     }
 
-    public double getLastStoppedTime() {
-        return lastStoppedTime;
+    public String result() {
+        return (stop() / 10000D) + "s";
     }
 }
