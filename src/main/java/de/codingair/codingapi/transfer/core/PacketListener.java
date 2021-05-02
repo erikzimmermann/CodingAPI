df7c9861a161293795b9329ed86c8ee906cb0034
Erik Zimmermann <mail@erikzimmermann.de>
2021-05-02 16:44:30 +0200

    Remove packet API Can be found in the Packet lib here: https://github.com/CodingAir/PacketManagement

--- src/main/java/de/codingair/codingapi/transfer/core/PacketListener.java
+++
@@ -1,9 +0,0 @@
-package de.codingair.codingapi.transfer.core;
-
-import de.codingair.codingapi.transfer.packets.utils.Packet;
-
-public interface PacketListener<P> {
-    void onReceive(Packet packet, P player);
-
-    boolean onSend(Packet packet);
-}
