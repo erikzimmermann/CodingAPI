c148a7e11c17d636aec0cae1c2eb0035f4fb79f2
Erik Zimmermann <mail@erikzimmermann.de>
2021-06-18 16:44:35 +0200

    Fix inventory utils for 1.17

--- src/main/java/de/codingair/codingapi/player/gui/inventory/InventoryUtils.java
+++ src/main/java/de/codingair/codingapi/player/gui/inventory/InventoryUtils.java
@@ -10,43 +10,50 @@ import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
 public class InventoryUtils {
-    private static final Class<?> containerClass;
-    private static IReflection.FieldAccessor<?> title;
-    private static final IReflection.MethodAccessor updateInventory;
-    private static final IReflection.FieldAccessor<?> activeContainer;
-    private static final Class<?> packetPlayOutOpenWindowClass;
-    private static final IReflection.FieldAccessor<Integer> windowId;
-    private static final IReflection.ConstructorAccessor packetConstructor;
+    private static final Class<?> CONTAINER_CLASS;
+    private static final IReflection.FieldAccessor<?> TITLE;
+    private static final IReflection.MethodAccessor UPDATE_INVENTORY;
+    private static final IReflection.FieldAccessor<?> ACTIVE_CONTAINER;
+    private static final Class<?> PACKET_PLAY_OUT_OPEN_WINDOW_CLASS;
+    private static final IReflection.FieldAccessor<Integer> WINDOW_ID;
+    private static final IReflection.ConstructorAccessor PACKET_CONSTRUCTOR;
 
     static {
-        packetPlayOutOpenWindowClass = IReflection.getClass(IReflection.ServerPacket.PACKETS, "PacketPlayOutOpenWindow");
+        PACKET_PLAY_OUT_OPEN_WINDOW_CLASS = IReflection.getClass(IReflection.ServerPacket.PACKETS, "PacketPlayOutOpenWindow");
 
-        if (packetPlayOutOpenWindowClass != null) {
-            //initialize classes
-            containerClass = IReflection.getClass(IReflection.ServerPacket.INVENTORY, "Container");
-            updateInventory = IReflection.getMethod(PacketUtils.EntityPlayerClass, "updateInventory", new Class[] {containerClass});
-            activeContainer = IReflection.getField(PacketUtils.EntityHumanClass, "activeContainer");
-            windowId = IReflection.getField(containerClass, "windowId");
+        if (PACKET_PLAY_OUT_OPEN_WINDOW_CLASS != null) {
+            CONTAINER_CLASS = IReflection.getClass(IReflection.ServerPacket.INVENTORY, "Container");
+
+            if (Version.atLeast(17)) {
+                UPDATE_INVENTORY = IReflection.getMethod(CONTAINER_CLASS, "updateInventory");
+            } else {
+                UPDATE_INVENTORY = IReflection.getMethod(PacketUtils.EntityPlayerClass, "updateInventory", new Class[] {CONTAINER_CLASS});
+            }
+
+            ACTIVE_CONTAINER = IReflection.getField(PacketUtils.EntityHumanClass, Version.since(17, "activeContainer", "bV"));
+            WINDOW_ID = IReflection.getField(CONTAINER_CLASS, Version.since(17, "windowId", "j"));
 
             if (Version.get().isBiggerThan(Version.v1_13)) {
                 Class<?> containersClass = IReflection.getClass(IReflection.ServerPacket.INVENTORY, "Containers");
-                title = IReflection.getField(containerClass, "title");
+                TITLE = IReflection.getField(CONTAINER_CLASS, "title");
 
-                packetConstructor = IReflection.getConstructor(packetPlayOutOpenWindowClass, int.class, containersClass, PacketUtils.IChatBaseComponentClass);
+                PACKET_CONSTRUCTOR = IReflection.getConstructor(PACKET_PLAY_OUT_OPEN_WINDOW_CLASS, int.class, containersClass, PacketUtils.IChatBaseComponentClass);
             } else {
-                packetConstructor = IReflection.getConstructor(packetPlayOutOpenWindowClass, int.class, String.class, PacketUtils.IChatBaseComponentClass, int.class);
+                TITLE = null;
+                PACKET_CONSTRUCTOR = IReflection.getConstructor(PACKET_PLAY_OUT_OPEN_WINDOW_CLASS, int.class, String.class, PacketUtils.IChatBaseComponentClass, int.class);
             }
         } else {
-            containerClass = null;
-            updateInventory = null;
-            activeContainer = null;
-            windowId = null;
-            packetConstructor = null;
+            CONTAINER_CLASS = null;
+            UPDATE_INVENTORY = null;
+            ACTIVE_CONTAINER = null;
+            WINDOW_ID = null;
+            PACKET_CONSTRUCTOR = null;
+            TITLE = null;
         }
     }
 
     private static boolean isReady() {
-        return packetPlayOutOpenWindowClass != null;
+        return PACKET_PLAY_OUT_OPEN_WINDOW_CLASS != null;
     }
 
     public static void updateTitle(@NotNull Player player, @NotNull String title, @NotNull Inventory inventory) {
@@ -58,24 +65,28 @@ public class InventoryUtils {
         Object openWindowPacket = preparePacket(activeContainer, title, inventory);
         PacketUtils.sendPacket(player, openWindowPacket);
 
-        updateInventory.invoke(entityPlayer, activeContainer);
+        if (Version.atLeast(17)) {
+            UPDATE_INVENTORY.invoke(activeContainer);
+        } else {
+            UPDATE_INVENTORY.invoke(entityPlayer, activeContainer);
+        }
     }
 
     private static Object getActiveContainer(Object entityPlayer) {
-        return activeContainer.get(entityPlayer);
+        return ACTIVE_CONTAINER.get(entityPlayer);
     }
 
     private static Object preparePacket(@NotNull Object activeContainer, @NotNull String title, @NotNull Inventory inventory) {
         Object messageComponent = getTitleComponent(title);
         int size = inventory.getSize();
-        int id = windowId.get(activeContainer);
+        int id = WINDOW_ID.get(activeContainer);
 
         Object packet;
         if (Version.get().isBiggerThan(Version.v1_13)) {
-            InventoryUtils.title.set(activeContainer, messageComponent);
-            packet = packetConstructor.newInstance(id, getContainerType(size), messageComponent);
+            InventoryUtils.TITLE.set(activeContainer, messageComponent);
+            packet = PACKET_CONSTRUCTOR.newInstance(id, getContainerType(size), messageComponent);
         } else {
-            packet = packetConstructor.newInstance(id, "minecraft:chest", messageComponent, size);
+            packet = PACKET_CONSTRUCTOR.newInstance(id, "minecraft:chest", messageComponent, size);
         }
 
         return packet;
@@ -88,7 +99,29 @@ public class InventoryUtils {
 
     private static Object getContainerType(int size) {
         Class<?> containersClass = IReflection.getClass(IReflection.ServerPacket.INVENTORY, "Containers");
-        IReflection.FieldAccessor<?> generic = IReflection.getField(containersClass, "GENERIC_9X" + (size / 9));
+        IReflection.FieldAccessor<?> generic = IReflection.getField(containersClass, getContainerTypeName(size));
         return generic.get(null);
+    }
+
+    private static String getContainerTypeName(int size) {
+        int id = size / 9;
+
+        if (Version.atLeast(17)) {
+            switch (id) {
+                case 1:
+                    return "a";
+                case 2:
+                    return "b";
+                case 3:
+                    return "c";
+                case 4:
+                    return "d";
+                case 5:
+                    return "e";
+                case 6:
+                    return "f";
+                default: throw new IllegalArgumentException("Cannot find an inventory type for size: " + size);
+            }
+        } else return "GENERIC_9X" + id;
     }
 }
