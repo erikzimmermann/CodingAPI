4d9c2a9087ed9b7c1d1a7a241c7744b3ac5fc027
Erik Zimmermann <mail@erikzimmermann.de>
2021-07-15 12:48:32 +0200

    Add pupur as server type Update version to v1.29

--- src/main/java/de/codingair/codingapi/server/specification/Type.java
+++ src/main/java/de/codingair/codingapi/server/specification/Type.java
@@ -1,12 +1,16 @@
 package de.codingair.codingapi.server.specification;
 
+import de.codingair.codingapi.API;
+
+import java.util.logging.Level;
+
 public enum Type {
     BUKKIT("Bukkit"),
     SPIGOT("Spigot"),
     PAPER("Paper"),
     PAPER_SPIGOT("PaperSpigot"),
-    UNKNOWN()
-    ;
+    PURPUR("Purpur"),
+    UNKNOWN("Unknown");
 
     private final String name;
 
@@ -14,22 +18,24 @@ public enum Type {
         this.name = name;
     }
 
-    Type() {
-        this(null);
+    public static Type getByName(String name) {
+        if (name == null) return UNKNOWN;
+
+        for (Type value : values()) {
+            if (value == UNKNOWN) continue;
+            if (value.getName().equals(name)) return value;
+        }
+
+        if (API.getInstance().getMainPlugin() != null) API.getInstance().getMainPlugin().getLogger().log(Level.INFO, "Could not detect server type \"" + name + "\".");
+        return UNKNOWN;
     }
 
     public String getName() {
         return name;
     }
 
-    public static Type getByName(String name) {
-        if(name == null) return UNKNOWN;
-
-        for(Type value : values()) {
-            if(value == UNKNOWN) continue;
-            if(value.getName().equals(name)) return value;
-        }
-
-        return UNKNOWN;
+    @Override
+    public String toString() {
+        return name;
     }
 }
