a9de66b199c52398866297d826b738867457a800
Erik Zimmermann <mail@erikzimmermann.de>
2023-01-11 10:38:01 +0100

    Allow custom titles for AnvilGUI

--- src/main/java/de/codingair/codingapi/player/gui/inventory/v2/buttons/AnvilButton.java
+++ src/main/java/de/codingair/codingapi/player/gui/inventory/v2/buttons/AnvilButton.java
@@ -5,13 +5,18 @@ import de.codingair.codingapi.player.gui.inventory.v2.GUI;
 import de.codingair.codingapi.tools.Call;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class AnvilButton extends Button implements GUISwitchButton {
+    private String title = null;
+
     public abstract void onAnvil(GUI fallback, AnvilClickEvent e);
 
     public abstract ItemStack buildAnvilItem();
 
-    public boolean open(GUI gui, Call call, String title) {
+    @Override
+    public boolean open(ClickType clickType, GUI gui, Call call) {
         AnvilGUI.openAnvil(gui.getPlugin(), gui.getPlayer(), new AnvilListener() {
             @Override
             public void onClick(AnvilClickEvent e) {
@@ -32,8 +37,14 @@ public abstract class AnvilButton extends Button implements GUISwitchButton {
         return false;
     }
 
-    @Override
-    public boolean open(ClickType clickType, GUI gui, Call call) {
-        return open(gui, call, null);
+    @Nullable
+    public String getTitle() {
+        return title;
+    }
+
+    @NotNull
+    public AnvilButton setTitle(@Nullable String title) {
+        this.title = title;
+        return this;
     }
 }
